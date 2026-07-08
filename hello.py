e2aaed4628354936e7db7af74293aad3ba14e484
oomale <omalegrace2009@gmail.com>
2026-07-08 10:57:52 +0100

    python basics

---
+++ hello.py
@@ -0,0 +1,21 @@
+# print to the console
+print("Hello, World!")
+
+# import the "sys" package to check the python version of the system
+import sys
+print (sys.version)
+
+# checking/learning indentation
+if 1 < 2:
+    print('i am correct')
+
+# learning variable declaration
+name = "grace"
+age = 10
+print(name, age)
+
+# connecting different syntax/code on same line
+print(1); print("true"); print("alter")
+print("Hello World!", end=" ")
+print("Oh no!")
+print("Hello World!", "Oh no!")
